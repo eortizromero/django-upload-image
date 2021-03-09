cdc5e87d37b6aba9ca84cb6650f3ba3cda55946a
Edgardo Ortiz <edgardoficial.yo@gmail.com>
2021-03-09 10:08:14 -0600

    Forms, Lista de imagenes y modelos

--- home/forms.py
+++ home/forms.py
@@ -6,4 +6,4 @@ class ImageForm(forms.ModelForm):
     """ Form para el modelo images"""
     class Meta:
         model = Images
-        fields = ('titulo', 'imagen') 
+        fields = ('titulo', 'imagen', 'descripcion') 
--- home/models.py
+++ home/models.py
@@ -5,6 +5,7 @@ class Images(models.Model):
 
     titulo = models.CharField(max_length=250)
     imagen = models.ImageField(upload_to='images/%Y/%m/%d/', blank=True)
+    descripcion = models.TextField(default="")
 
     def __str__(self):
         return self.titulo
